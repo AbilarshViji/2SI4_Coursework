7503e239a2a9dda1ccc4530027b7623e0bf7b0dc
Abilarsh Viji <AbilarshViji@users.noreply.github.com>
2019-01-30 16:08:58 -0500

    Fix Constructors

--- HugeInteger/src/hugeinteger/HugeInteger.java
+++ HugeInteger/src/hugeinteger/HugeInteger.java
@@ -15,38 +15,44 @@ public class HugeInteger {
 
         //Check if there is something in the string input
         if (val.length() == 0 || val.charAt(0) == '-' && val.length() == 1) {
-            throw new NumberFormatException("Bad Input");
+            throw new NumberFormatException("Bad Input, not big enough");
         }
 
-            int i = 0;
-            if (val.charAt(0) == '-') {
-                neg = true;
-                i = 1;
-            }
+        int i = 0;
+        if (val.charAt(0) == '-') {
+            neg = true;
+            i = 1;
+        }
+        try {
             for (; i < val.length(); i++) {
-                if (Character.getNumericValue(val.charAt(i)) > 9 || Character.getNumericValue(val.charAt(i)) <= 0) {
-                    throw new NumberFormatException("There was a non-int in your input (0-9)");
-                }
+                Integer.parseInt(Character.toString(val.charAt(i)));
                 System.out.println(Character.getNumericValue(val.charAt(i)));
                 hugeIntSB.append(val.charAt(i));
             }
-            hugeInt = hugeIntSB.toString();
- 
+        } catch (NumberFormatException e) {
+            System.out.println("There was a non-int in your input (0-9)");
+            throw e;
+        } catch (Exception e) {
+            System.out.println("Figure out how this happened");
+            throw e;
+        }
+        hugeInt = hugeIntSB.toString();
+
     }
-    
+
     public HugeInteger(int n) {
-        if (n<1){
+        if (n < 1) {
             throw new NumberFormatException("Int must be greater then 0");
         }
         StringBuilder hugeIntSB = new StringBuilder();
         Random rand = new Random();
-        hugeIntSB.append(Integer.toString(rand.nextInt(8)+1));
-        for(int i=1; i < n; i++){
-            hugeIntSB.append(Integer.toString(rand.nextInt(9)));
+        hugeIntSB.append(Integer.toString(rand.nextInt(9) + 1));
+        for (int i = 1; i < n; i++) {
+            hugeIntSB.append(Integer.toString(rand.nextInt(10)));
         }
         hugeInt = hugeIntSB.toString();
     }
-/*
+    /*
     public HugeInteger add(HugeInteger h) {
 
     }
