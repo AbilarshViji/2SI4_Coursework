4d5003fc1d310fb060b3518e1a339aac3435eb9d
Abilarsh <AbilarshViji@users.noreply.github.com>
2019-02-25 13:16:58 -0500

    init lab 3 BST

---
+++ BinarySearchTree/src/com/abilarsh/BSTSet.java
@@ -0,0 +1,73 @@
+package com.abilarsh;
+
+public class BSTSet {
+    private TNode root;
+    
+    public BSTSet(){
+        
+    }
+    
+    public BSTSet(int [] input){
+        
+    }
+    
+    public boolean isIn(int v){
+        
+    }
+    
+    public void add(int v){
+        
+    }
+    
+    public boolean remove(int v){
+        
+    }
+    
+    public boolean remove(int v){
+        
+    }
+    
+    public BSTSet union(BSTSet s){
+        
+    }
+    
+    public BSTSet intersection(BSTSet s){
+        
+    }
+    
+    public BSTSet difference(BSTSet s){
+        
+    }
+    
+    public int site(){
+        
+    }
+    
+    public int height(){
+        
+    }
+    
+    public void printBSTset(){
+        if(root==null){
+            System.out.println("This set it empty");
+        }
+        else{
+            System.out.println("the set elements are: ");
+            printBSTset(root);
+            System.out.println("\n");
+        }
+    }
+    
+    
+    public void printBSTset(TNode t){
+        if(t!=null){
+            printBSTset(t.left);
+            System.out.println(" "+ t.element + ", ");
+            printBSTset(t.right);
+        }
+    }
+    
+    public void printNonRec(){
+        // TODO: 25/02/19 This method is nonrecusrive and uses a stack to inplement the inorder traversal 
+    }
+}
---
+++ BinarySearchTree/src/com/abilarsh/TNode.java
@@ -0,0 +1,12 @@
+package com.abilarsh;
+
+public class TNode {
+    int element;
+    TNode left;
+    TNode right;
+    TNode (int i; TNode one; TNode r){
+        element = i;
+        left = one;
+        right = r;
+    }
+}
